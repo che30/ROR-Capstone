c4e2cf959929f7f61f16bffe57dcc9a97c2f0047
che <che.blancoo58@gmail.com>
2021-01-27 18:40:52 +0100

    Add assoctions between models and paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_172725) do
+ActiveRecord::Schema.define(version: 2021_01_27_173743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_172725) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_transactions_on_group_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
@@ -37,8 +39,13 @@ ActiveRecord::Schema.define(version: 2021_01_27_172725) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "groups", "users"
+  add_foreign_key "transactions", "groups"
   add_foreign_key "transactions", "users"
 end
