893cf0c49ee73ecc8eb907c431eb6586feccaf07
che <che.blancoo58@gmail.com>
2021-01-27 18:43:13 +0100

    Add user controller and  new and show views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user= User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    log_in @user
+      flash[:success] = 'user successfully created'
+      redirect_to @user
+    else
+      flash.now[:error] = "Error: #{@user.errors.full_messages.join(', ')}"
+      render 'new'
+    end
+  end
+  def show
+    @user=User.find(params[:id])
+  end
+  private
+  def user_params
+  params.require(:user).permit(:username,:avatar)
+  end
 end
