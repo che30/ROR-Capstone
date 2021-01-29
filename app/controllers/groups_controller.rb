c853ac6a55c3c92dbd5b996537c66ae661496402
che <che.blancoo58@gmail.com>
2021-01-29 10:38:52 +0100

    Update users new and show views update transaction no_group views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,35 @@
 class GroupsController < ApplicationController
   include GroupsHelper
-  before_action :authorize,only: [:new,:index]
+  before_action :authorize,only: %i[new index]
   def new
-    @group=Group.new
+    @group = Group.new
     @icons = icons
   end
-  def index
-    @groups=Group.all.order(name: 'ASC') 
-  end
-def create
-@group  = current_user.groups.build(group_params)
-if @group.save
-  flash[:success]='group created with succes'
-  redirect_to groups_path
-else
-flash.now[:alert]= "some errors while saving the form"
-  @icons = icons
-  render 'new'
-end
 
-end
-private
-def group_params
-  params.require(:group).permit(:name,:icon)
-end
+  def index
+    @groups = Group.all.order(name: 'ASC')
+  end
+
+  def create
+    @group  = current_user.groups.build(group_params)
+    if @group.save
+      flash[:success]='group created with succes'
+      redirect_to groups_path
+    else
+      flash.now[:alert] = 'some errors while saving the form'
+      @icons = icons
+      render 'new'
+    end
+  end
+
+  def show
+    @group = Group.find_by(id: params[:id])
+    @transactions = Transaction.includes(:group, :user).where(group_id: @group.id).order('created_at DESC')
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
