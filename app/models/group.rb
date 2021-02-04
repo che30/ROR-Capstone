4cc0c55f156faf6bbca921e2d39b2f0d15baa0d7
che <che.blancoo58@gmail.com>
2021-02-04 22:43:43 +0100

    Restyle homepage

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :icon, presence: true
+  has_many :transactions, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :transactions
-  has_many :groups
+  has_many :transactions, dependent: :destroy
+  has_many :groups, dependent: :destroy
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '50x50>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
