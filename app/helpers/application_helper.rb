783f280cc453afc6d1e5163fc0e109f5555ddb95
che <che.blancoo58@gmail.com>
2021-02-04 13:55:58 +0100

    Refactor application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def display_errors(model)
     return unless model.errors.full_messages.any?
 
-    content_tag :p, "model could not be saved. #{model.errors.full_messages.join('. ')}",
+    content_tag :p, "#{model.errors.full_messages.join('. ')}",
                 class: 'errors text-center alert alert-danger '
   end
 
