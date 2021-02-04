2ca08cfe4324078030bc7b608f2e75ec8adf435e
che <che.blancoo58@gmail.com>
2021-02-04 14:30:28 +0100

    Fix linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def display_errors(model)
     return unless model.errors.full_messages.any?
 
-    content_tag :p, "#{model.errors.full_messages.join('. ')}",
+    content_tag :p, model.errors.full_messages.join('. ').to_s,
                 class: 'errors text-center alert alert-danger '
   end
 
